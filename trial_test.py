e94562beddcfac3caac2c098ad33f8ed8e5d3659
Rey Sanayei <60049746+reys7899@users.noreply.github.com>
2023-09-29 09:14:38 -0700

    Split accuracy measure over the three tasks

--- trial_test.py
+++ trial_test.py
@@ -5,21 +5,15 @@ import numpy as np
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load the data from the JSON file
-with open('trial-v1.json', 'r') as file:
+with open('val.model-agnostic.json', 'r') as file:
     data = json.load(file)
 
 # Initialize the Sentence Transformer model
 model_name = 'bert-base-nli-mean-tokens'
 model = SentenceTransformer(model_name)
 
-# Initialize lists to store embeddings and labels
-source_hyp_embeddings = []
-source_tgt_embeddings = []
-true_labels = []
-predicted_labels = []
-
-# Set a similarity threshold
-similarity_threshold = 0.9
+# Set a similarity threshold (you can adjust this value)
+similarity_threshold = 0.925
 
 # Text preprocessing function
 def preprocess_text(text):
@@ -27,15 +21,19 @@ def preprocess_text(text):
     text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
     return text
 
-# Loop through each data point
+# Initialize dictionaries to store data for each task
+tasks = {'MT': [], 'DM': [], 'PG': []}
+
+# Loop through each data point and group by task
 for datapoint in data:
+    task = datapoint['task']
     source = preprocess_text(datapoint['src'])
     hypothesis = preprocess_text(datapoint['hyp'])
     target = preprocess_text(datapoint['tgt'])
 
     # Concatenate source and hypothesis
     source_hyp_text = f"{source} {hypothesis}"
-     # Concatenate source and target
+    # Concatenate source and target
     source_tgt_text = f"{source} {target}"
 
     # Encode the concatenated texts to get embeddings
@@ -53,18 +51,46 @@ for datapoint in data:
 
     true_label = datapoint['label']
 
-    # Append embeddings and labels
-    source_hyp_embeddings.append(source_hyp_embedding)
-    source_tgt_embeddings.append(source_tgt_embedding)
-    true_labels.append(true_label)
-    predicted_labels.append(predicted_label)
+    # Append data to the appropriate task
+    tasks[task].append({
+        'source_hyp_embedding': source_hyp_embedding,
+        'source_tgt_embedding': source_tgt_embedding,
+        'true_label': true_label,
+        'predicted_label': predicted_label
+    })
 
-# Calculate accuracy
-accuracy = accuracy_score(true_labels, predicted_labels)
+# Initialize dictionaries to store results for each task
+task_results = {}
 
-# Generate classification report
-classification_rep = classification_report(true_labels, predicted_labels, target_names=['Hallucination', 'Not Hallucination'])
+# Calculate accuracy for each task and total accuracy
+total_true_labels = []
+total_predicted_labels = []
 
-print(f"Accuracy: {accuracy}")
-print("Classification Report:")
-print(classification_rep)
+for task, task_data in tasks.items():
+    true_labels = [item['true_label'] for item in task_data]
+    predicted_labels = [item['predicted_label'] for item in task_data]
+
+    # Calculate accuracy for the current task
+    accuracy = accuracy_score(true_labels, predicted_labels)
+
+    task_results[task] = {
+        'accuracy': accuracy,
+        # 'classification_report': classification_report(true_labels, predicted_labels, target_names=['Hallucination', 'Not Hallucination'])
+    }
+
+    # Accumulate true and predicted labels for total accuracy
+    total_true_labels.extend(true_labels)
+    total_predicted_labels.extend(predicted_labels)
+
+# Calculate total accuracy
+total_accuracy = accuracy_score(total_true_labels, total_predicted_labels)
+
+# Print results for each task and total accuracy
+for task, results in task_results.items():
+    print(f"Task: {task}")
+    print(f"Accuracy: {results['accuracy']}")
+    # print("Classification Report:")
+    # print(results['classification_report'])
+    print("\n")
+
+print(f"Total Accuracy: {total_accuracy}")
